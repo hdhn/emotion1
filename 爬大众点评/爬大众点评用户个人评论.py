34b520458cc3e0f38c32c9df924245250a357445
黄锴健 <hkjznyg@163.com>
2020-11-24 14:42:41 +0800

    代码更新

--- 爬大众点评/爬大众点评用户个人评论.py
+++ 爬大众点评/爬大众点评用户个人评论.py
@@ -4,9 +4,10 @@ import re
 import requests,csv
 from pyquery import PyQuery as pq
 import time,random
+import pyautogui
 
 headers = {
-    'Cookie':'_lxsdk_cuid=1753bb608fbc8-0be3842885820b-6b111b7e-1fa400-1753bb608fcc8; _lxsdk=1753bb608fbc8-0be3842885820b-6b111b7e-1fa400-1753bb608fcc8; _hc.v=8429fd3f-f5ca-d6a9-fe0f-58cf71ca55b9.1603024588; s_ViewType=10; ua=%E5%8B%BF%E5%BF%98%E5%BF%83%E5%AE%89_9104; ctu=7fc965fa839279cab50ca6c42a998102e40f1aa3353081dc3b314009932e84da; cy=3; cye=hangzhou; fspop=test; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1605786702,1605928795,1606009699,1606009855; ll=7fd06e815b796be3df069dec7836c3df; uamo=13247877023; dper=537233be492fe99ffbd53e9970a73e1589b890d5aad237957a18b22219dccbba1d05b5c34c41455297eb0415ddc21e8a0b6a7cf9983d04a5be46c7d9a1bb042bc5e0c5a3b81bcb137e4f111395a7a219061ed350096dec166da7984f36e449c9; dplet=8a2b0fe39b448096803e21ebaff3c299; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1606135333; _lx_utm=utm_source%3Dwww.sogou%26utm_medium%3Dorganic; _lxsdk_s=175f5203f64-017-4e9-85a%7C%7C3',
+    'Cookie':'_lxsdk_cuid=1753bb608fbc8-0be3842885820b-6b111b7e-1fa400-1753bb608fcc8; _lxsdk=1753bb608fbc8-0be3842885820b-6b111b7e-1fa400-1753bb608fcc8; _hc.v=8429fd3f-f5ca-d6a9-fe0f-58cf71ca55b9.1603024588; s_ViewType=10; ua=%E5%8B%BF%E5%BF%98%E5%BF%83%E5%AE%89_9104; ctu=7fc965fa839279cab50ca6c42a998102e40f1aa3353081dc3b314009932e84da; cy=3; cye=hangzhou; fspop=test; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1605928795,1606009699,1606009855,1606186966; expand=yes; lgtoken=0cfaecc47-2b43-4c9d-a45d-9cacd4d37556; dplet=0b8c7f54e6cc7c65c96237d40aab53d3; dper=537233be492fe99ffbd53e9970a73e1534e104496da6aeb2ec9ff869c71ea5313115a88312c6fbfd3c1b99ca988c927dae358fa74256030cdb3d247dc1a3c13e94ee73620cef8dc47e318a02fc607e4a2c91c13c9b0b213dbb0c5a0b2faea7ca; ll=7fd06e815b796be3df069dec7836c3df; uamo=13247877023; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1606191586; _lx_utm=utm_source%3Dwww.sogou%26utm_medium%3Dorganic; _lxsdk_s=175f87a396b-283-59a-5fc%7C%7C27',
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36',
     'Referer': '{url}/reviews'
 }
@@ -19,7 +20,22 @@ def get_html(url):
     try:
         print(doc("div.user-info>span").attr('class').split(' ')[1].replace('urr-rank',''))
     except:
-        input("按enter键继续")
+        input('\033[33;46m请按enter键继续     \033[0m')
+        if pyautogui.pixelMatchesColor(209,972,(6,184,184)):
+            pyautogui.click(215, 1059, button='left')
+            time.sleep(2)
+            pyautogui.click(131, 45, button='left')
+            time.sleep(2)
+            pyautogui.moveTo(547, 329, duration=1)
+            pyautogui.dragTo(748, 331, duration=random.randint(1, 2))
+            time.sleep(2)
+            pyautogui.click(277, 1063, button='left')
+            time.sleep(2)
+            pyautogui.click(240, 978, button='left')
+            time.sleep(1)
+            pyautogui.keyDown('enter')
+            time.sleep(0.5)
+            pyautogui.keyUp('enter')
         response = requests.get(url =url,headers = headers)
         doc = pq(response.text)
     user_star = doc("div.user-info>span").attr('class').split(' ')[1].replace('urr-rank','')
